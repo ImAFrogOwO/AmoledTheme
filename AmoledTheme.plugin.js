6a005c29535d1a5dde12432b2d3802ca1f75b531
Nick <90235641+ImAFrogOwO@users.noreply.github.com>
2022-10-22 15:54:29 -0500

    Create AmoledTheme.plugin.js

---
+++ AmoledTheme.plugin.js
@@ -0,0 +1,22 @@
+/**
+ * @name Amoled Theme
+ * @description Enable/Disable Androids Dark Dark Mode.
+ * @author Barvo#4061
+ * @version 1.0.0
+ */
+
+module.exports = class {
+	getName() { return "Amoled Theme"; }
+
+	start() {
+
+		document.body.classList.value = ("theme-amoled");
+	}
+
+	stop() {
+
+		document.body.classList.value = ("theme-dark");
+
+    }
+
+};
